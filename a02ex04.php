f0653831f20488e7e4e7320abc1049383ec16134
Mauro <maurojh@gmail.com>
2019-08-13 19:13:21 -0300

    Teste 2

---
+++ a02ex04.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <title>Aula 2 - Exercício 4</title>
+</head>
+
+<body>
+    
+    <form action="recebe_a02ex04.php">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <div class="field">
+                        <div class="field-label">
+                            <label class="label">X:</label>
+                        </div>
+                        <div class="field-body">
+                            <div class="field">
+                                <p class="control">
+                                    <input class="input" type="text" name="x">
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="field">
+                        <div class="field-label">
+                            <label class="label">Y:</label>
+                        </div>
+                        <div class="field-body">
+                            <div class="field">
+                                <p class="control">
+                                    <input class="input" type="text" name="y">
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="field">
+                        <div class="field-body">
+                            <div class="field">
+                                <p class="control">
+                                    <input class="button" type="submit" value="Enviar">
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    
+</body>
+
+</html>
